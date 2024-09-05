7fcf913e86e00de55cd7fad7f5cba4519d674e02
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-09-05 18:42:17 +0200

    created blueprint of MathEngine class

---
+++ src/main/java/com/example/simplecalulatorproject/MathEngine.java
@@ -0,0 +1,34 @@
+package com.example.simplecalulatorproject;
+
+class MathEngine {
+    void numberPressed(int pressedNumber)
+    {
+        System.out.println(pressedNumber);
+    }
+
+    void plusPressed() {
+        System.out.println("+");
+    }
+
+    void minusPressed() {
+    }
+
+    void muliplyPressed() {
+    }
+
+    void dividePressed() {
+    }
+
+    void equalsPressed() {
+    }
+
+    void dotPressed() {
+    }
+
+    String getScreenValue()
+    {
+        return "1234";
+    }
+
+
+}
