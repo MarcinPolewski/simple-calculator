159c42552a59ff9eadeee2faa2d0aa7c79de61a1
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-09-05 18:41:51 +0200

    updated MainView(.fxml file) by adding missing buttons, started implementing handing button interactions

--- src/main/java/com/example/simplecalulatorproject/MainSceneController.java
+++ src/main/java/com/example/simplecalulatorproject/MainSceneController.java
@@ -1,14 +1,100 @@
 package com.example.simplecalulatorproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainSceneController {
     @FXML
-    private Label welcomeText;
+    Label mainScreenLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0, buttonPlus, buttonMinus, buttonDivision, buttonMultiplication, buttonDot, buttonEquals;
+
+    MathEngine mathEngine = new MathEngine();
+
+    private void updateScreen()
+    {
+        mainScreenLabel.setText(mathEngine.getScreenValue());
     }
+
+    public void numberButtonPressed(ActionEvent event)
+    {
+        if(event.getSource() == button0)
+        {
+            mathEngine.numberPressed(0);
+        }
+        else if(event.getSource() == button1)
+        {
+            mathEngine.numberPressed(1);
+        }
+        else if(event.getSource() == button2)
+        {
+            mathEngine.numberPressed(2);
+        }
+        else if(event.getSource() == button3)
+        {
+            mathEngine.numberPressed(3);
+        }
+        else if(event.getSource() == button4)
+        {
+            mathEngine.numberPressed(4);
+        }
+        else if(event.getSource() == button5)
+        {
+            mathEngine.numberPressed(5);
+        }
+        else if(event.getSource() == button6)
+        {
+            mathEngine.numberPressed(6);
+        }
+        else if(event.getSource() == button7)
+        {
+            mathEngine.numberPressed(7);
+        }
+        else if(event.getSource() == button8)
+        {
+            mathEngine.numberPressed(8);
+        }
+        else if(event.getSource() == button9)
+        {
+            mathEngine.numberPressed(9);
+        }
+        updateScreen();
+    }
+    public void plusButtonPressed(ActionEvent event)
+    {
+        mathEngine.plusPressed();
+        updateScreen();
+    }
+    public void minusButtonPressed(ActionEvent event)
+    {
+        mathEngine.minusPressed();
+        updateScreen();
+
+    }
+    public void multiplyButtonPressed(ActionEvent event)
+    {
+        mathEngine.muliplyPressed();
+        updateScreen();
+
+    }
+    public void divideButtonPressed(ActionEvent event)
+    {
+        mathEngine.dividePressed();
+        updateScreen();
+    }
+    public void equalsButtonPressed(ActionEvent event)
+    {
+        mathEngine.equalsPressed();
+        updateScreen();
+    }
+    public void dotButtonPressed(ActionEvent event)
+    {
+        mathEngine.dotPressed();
+        updateScreen();
+    }
+
+
 }
